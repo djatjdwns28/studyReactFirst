3b281991009cacc678a0f1cccea7aa5c6377fe30
djatjdwns <djatjdwns28@gmail.com>
2023-02-07 01:09:09 +0900

    first

---
+++ react-for-beginners/src/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import Movie from './Movie'
+import styles from './App.module.css'
+
+
+function App() {
+  return null
+}
+
+export default App;
